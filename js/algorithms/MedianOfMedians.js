7efcdc421eb09bb95bbd5b62a12024a0eaf0e4ad
Philip Anderson <panderson160@gmail.com>
2016-07-29 12:44:58 -0400

    Median of medians with small recursion depth problem

--- js/algorithms/MedianOfMedians.js
+++ js/algorithms/MedianOfMedians.js
@@ -3,6 +3,100 @@
  */
 
 
-function findMedian(list, comparator) {
+var chunkSize = 3;
+var medianOfChunk = medianOf3;
 
+function findMedian(list, comparator) {
+    if (list.length === 0)
+        return null;
+    if (list.length === 1)
+        return list[0];
+
+    return quickSelect(list, Math.floor(list.length / 2), comparator);
+}
+
+function findMedianOfMedians(list, comparator) {
+    if (list.length <= chunkSize) {
+        var median = medianOfChunk(list);
+        return medianOfChunk(list);
+    }
+
+    var chunks = [];
+    var numChunks = Math.floor(list.length / chunkSize);
+    var mediansOfChunks = [];
+
+    for (var i = 0; i < numChunks; i++) {
+        mediansOfChunks.push(medianOfChunk(list.slice(i * chunkSize, i * chunkSize + chunkSize)));
+    }
+
+    return findMedian(mediansOfChunks, comparator);
+}
+
+function quickSelect(list, k, comparator) {
+    if (list.length === 1)
+        return list[0];
+
+    console.log("Quick select of " + k + "/" + list.length);
+
+    var medianOfMedians = findMedianOfMedians(list, comparator);
+
+    var lessThan = [];
+    var greaterThan = [];
+    var singleValue = true;
+    for (var i = 0; i < list.length; i++) {
+        if (singleValue && list[i] !== list[0])
+            singleValue = false;
+        if (list[i] <= medianOfMedians)
+            lessThan.push(list[i]);
+        else
+            greaterThan.push(list[i]);
+    }
+
+    if (singleValue)
+        return list[0];
+
+    if (lessThan.length > k)
+        return quickSelect(lessThan, k, comparator);
+    else
+        return quickSelect(greaterThan, k - lessThan.length, comparator);
+}
+
+function medianOf1(chunk) {
+    return chunk[0];
+}
+
+function medianOf2(chunk) {
+    if (chunk.length < 2)
+        return medianOf1(chunk);
+    var a = chunk[0];
+    var b = chunk[1];
+
+    return (a + b) / 2;
+}
+
+function medianOf3(chunk) {
+    if (chunk.length < 3)
+        return medianOf2(chunk);
+
+    var a = chunk[0];
+    var b = chunk[1];
+    var c = chunk[2];
+
+    if (a < b) {
+        if (b < c) {
+            return b;
+        } else if (a < c) {
+            return c;
+        } else {
+            return a;
+        }
+    } else {
+        if (a < c) {
+            return a;
+        } else if (b < c) {
+            return c;
+        } else {
+            return b;
+        }
+    }
 }
--- tests/MoM_test.js
+++ tests/MoM_test.js
@@ -9,7 +9,7 @@ test("simple test", function () {
     list.push(3);
     list.push(5);
 
-    var result = findMedian(list);
+    var result = quickSelect(list, 1);
 
     equal(result, 3);
 });
@@ -17,32 +17,76 @@ test("simple test", function () {
 
 test("simple test unordered", function () {
     var list = [];
-    list.push(6);
     list.push(9);
+    list.push(6);
     list.push(1);
     list.push(5);
     list.push(7);
     list.push(2);
-    list.push(4);
     list.push(3);
     list.push(8);
+    list.push(4);
 
-    var result = findMedian(list);
+    var result = quickSelect(list, 4);
 
     equal(result, 5);
 });
 
 
+var largeTestSize = 4000000;
 test("large test (randomized)", function () {
     var list = [];
 
-    for(var i = 0; i < 1000; i++) {
+    for (var i = 0; i < largeTestSize; i++) {
         list.push(Math.random() * 1000);
         list.push(Math.random() * 1000 + 10000);
     }
     list.push(5000);
 
-    var result = findMedian(list);
+    var result = quickSelect(list, largeTestSize);
 
     equal(result, 5000);
+});
+
+
+test("large test (by sortation)", function () {
+    var list = [];
+
+    for (var i = 0; i < largeTestSize; i++) {
+        list.push(Math.random() * 1000);
+        list.push(Math.random() * 1000 + 10000);
+    }
+    list.push(5000);
+
+    list = list.sort(d3.ascending);
+    var result = list[largeTestSize];
+
+    equal(result, 5000);
+});
+
+test("Variable test size", function () {
+    var testKSize = function (k) {
+        var list = [];
+
+        for (var i = 0; i < k; i++) {
+            list.push(Math.random() * 1000);
+            list.push(Math.random() * 1000 + 10000);
+        }
+        list.push(5000);
+
+        var result = quickSelect(list, k);
+
+        equal(result, 5000);
+    };
+
+    var maxTestSize = 4000000;
+    var results = [];
+    for (var k = 1; k <= maxTestSize; k *= 2) {
+        console.log(k);
+        var t0 = performance.now();
+        testKSize(k);
+        var t1 = performance.now();
+        results.push(t1 - t0);
+    }
+    console.log(results);
 });
